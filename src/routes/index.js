49abfa32552b073edb045c1239b19534415ed637
WillBorged1733 <134107458+ayhamyza@users.noreply.github.com>
2024-06-11 20:06:56 -0300

    authentication routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,14 @@ const {Router} = require("express")
 const usersRouter = require("./users.routes")
 const notesRouter = require("./notes.routes")
 const tagsRouter = require("./tags.routes")
-
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router()
 
-routes.use ("/users", usersRouter) //TODA VEZ QUE O /users FOR ACESSADO SERA REDIRECIONADO PARA users.routes.js
-routes.use ("/notes", notesRouter)
-routes.use ("/tags", tagsRouter)
+routes.use ("/users", usersRouter); //TODA VEZ QUE O /users FOR ACESSADO SERA REDIRECIONADO PARA users.routes.js
+routes.use("/sessions" , sessionsRouter);
+routes.use ("/notes", notesRouter);
+routes.use ("/tags", tagsRouter);
 
 
 module.exports = routes; // exporta todos os grupos de rotas da aplicação
